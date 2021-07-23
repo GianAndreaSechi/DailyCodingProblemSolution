df93d0b8467935a1344463075ee3d486ebd72af4
GianAndreaSechi <65762192+GianAndreaSechi@users.noreply.github.com>
2021-07-23 14:56:31 +0200

    Update solution.py

--- 1 - Google/solution.py
+++ 1 - Google/solution.py
@@ -2,13 +2,25 @@ def main():
     k = 17
     X = [10, 15, 3, 7]
 
-    print(isSummable(k,X,0))
-
+    print("First Solution: ", isSummable(k,X,0))
+    print("Second Solution: ", isSummableBis(k,X))
+#O(N^2) time
+#O(1) space
 def isSummable(k, X, p):
     for v_f in X:
         for v_s in range(p, len(X)):
             if v_f + v_s == k: return True
         p =  X.index(v_f)
 
+    return False
+
+#O(N Log N) time
+#O(1) space
+def isSummableBis(k, X):
+    l_seen = set()
+    for v in X:
+        if k - v in l_seen:
+            return True
+        l_seen.add(v)
     return False
 main()
